ca26245df90e54da4b1fabc141066ce79cb8d917
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-07 11:47:38 +0530

    first commit

---
+++ RainbowSchoolP/Data/RainbowSchoolPDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RainbowSchoolP.Model;
+
+namespace RainbowSchoolP.Data
+{
+    public class RainbowSchoolPDbContext : DbContext
+    {
+        public RainbowSchoolPDbContext (DbContextOptions<RainbowSchoolPDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RainbowSchoolP.Model.RainBowSchool> RainBowSchool { get; set; } = default!;
+    }
+}
---
+++ RainbowSchoolP/Model/RainBowSchool.cs
@@ -0,0 +1,10 @@
+﻿namespace RainbowSchoolP.Model
+{
+    public class RainBowSchool
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Subject { get; set; }
+        public double SubjectMarks { get; set; }
+    }
+}
